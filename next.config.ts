21b9930e1bf595a3720ca1ef1e3b9c8b88fc300b
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-21 19:33:58 +0100

    installed voxx for blog management, updated next config to allow for voxx to run properly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import { withVoxx } from "@prudentbird/voxx-core/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withVoxx(nextConfig);
