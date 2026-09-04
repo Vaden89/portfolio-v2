a4613bf64ca84a0def242510493960f652148d00
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-09-04 08:56:43 +0100

    added theme switcher

---
+++ src/components/common/theme-toggle.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { flushSync } from "react-dom";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  function toggleTheme() {
+    const nextTheme = theme === "light" ? "dark" : "light";
+
+    if (!document.startViewTransition) {
+      setTheme(nextTheme);
+      return;
+    }
+
+    document.startViewTransition(() => {
+      flushSync(() => setTheme(nextTheme));
+    });
+  }
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? <Sun size={18} /> : <Moon size={18} />}
+    </button>
+  );
+}
